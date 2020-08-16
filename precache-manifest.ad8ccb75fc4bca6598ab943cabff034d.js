55fe2c77d160594c97b84981e464d5ee2c2e4382
anniephilip23 <anniephilip23@gmail.com>
2020-08-16 22:55:19 +0530

    Updates

---
+++ precache-manifest.ad8ccb75fc4bca6598ab943cabff034d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65d003fc8507c9e6c334c1643c88554",
+    "url": "/A_protofolio/index.html"
+  },
+  {
+    "revision": "e5a8a302169374acca92",
+    "url": "/A_protofolio/static/css/main.5412cd93.chunk.css"
+  },
+  {
+    "revision": "30a712a6912dea92f566",
+    "url": "/A_protofolio/static/js/2.9f025a28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/A_protofolio/static/js/2.9f025a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a8a302169374acca92",
+    "url": "/A_protofolio/static/js/main.9ee4a7cf.chunk.js"
+  },
+  {
+    "revision": "d12ace36d886b8ef659f",
+    "url": "/A_protofolio/static/js/runtime-main.606b7305.js"
+  },
+  {
+    "revision": "dd52ff34e53ae456aa02a9fb747e98a8",
+    "url": "/A_protofolio/static/media/A.dd52ff34.png"
+  },
+  {
+    "revision": "54320817718de291a73c071cc9d26640",
+    "url": "/A_protofolio/static/media/abt.54320817.png"
+  },
+  {
+    "revision": "7522e4264f78a6dada04e51f544949e6",
+    "url": "/A_protofolio/static/media/angularjs.7522e426.png"
+  },
+  {
+    "revision": "f3b0dbc43684f0e9a9a64be3b759a056",
+    "url": "/A_protofolio/static/media/covid.f3b0dbc4.jpg"
+  },
+  {
+    "revision": "3180a1faad3ccf642ffcbca60471c6b3",
+    "url": "/A_protofolio/static/media/insure.3180a1fa.jpg"
+  },
+  {
+    "revision": "b502449ea2692788907ff6f342458242",
+    "url": "/A_protofolio/static/media/js.b502449e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/A_protofolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8afa85b9e770ae1c6ab178283ed232c7",
+    "url": "/A_protofolio/static/media/memory.8afa85b9.png"
+  },
+  {
+    "revision": "9100f2eb47cae949fb1f01853263103c",
+    "url": "/A_protofolio/static/media/node.9100f2eb.png"
+  },
+  {
+    "revision": "9084ee0f87264199698b25e6c0ea7af6",
+    "url": "/A_protofolio/static/media/proj.9084ee0f.png"
+  },
+  {
+    "revision": "638272649255f64f39256478c22cad4e",
+    "url": "/A_protofolio/static/media/quiz.63827264.jpg"
+  },
+  {
+    "revision": "893ec8180b4646ea2d68eb07552f3a2c",
+    "url": "/A_protofolio/static/media/repo.893ec818.png"
+  },
+  {
+    "revision": "4e4ba1f6f0d10a724515c283a998c536",
+    "url": "/A_protofolio/static/media/skill.4e4ba1f6.png"
+  },
+  {
+    "revision": "10bff956c61d12ec45e7ce28aa90de0e",
+    "url": "/A_protofolio/static/media/tictac.10bff956.jpg"
+  },
+  {
+    "revision": "10d0da7ba19ec516dc7f5f4864ecbb86",
+    "url": "/A_protofolio/static/media/todo.10d0da7b.jpg"
+  }
+]);
